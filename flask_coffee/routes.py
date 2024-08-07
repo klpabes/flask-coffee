603a9ce4ffe455922ad64e1dffa101d64ca3814a
klpabes <klpabes99@gmail.com>
2024-08-07 09:54:36 +0800

    add index_items to index route

--- flask_coffee/routes.py
+++ flask_coffee/routes.py
@@ -2,9 +2,28 @@ from flask import render_template, url_for, flash, redirect
 from flask_coffee import app
 # from app.forms import RegistrationForm, LoginForm
 
+index_items = [
+    {
+        'item': 'Latte',
+        'desc': 'Popular coffee drink made with espresso and steamed milk.',
+        'date_posted': 'August 6, 2024'
+    },
+    {
+        'item': 'Capuccino',
+        'desc': 'A classic coffee beverage made with equal parts espresso, steamed milk, and milk foam.',
+        'date_posted': 'August 6, 2024'
+    },
+    {
+        'item': 'Capuccino',
+        'desc': 'A classic coffee drink by diluting a shot of espresso with hot water.',
+        'date_posted': 'August 6, 2024'
+    },
+]
+
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', index_items=index_items)
 
 @app.route("/about")
 def about():
